e1ea5d997c32740357c7d52975f6d50281734fa9
ZhengHongyi <111111@163.com>
2023-11-09 08:38:35 +0800

    1

--- vue-admin-template/src/views/login/index.vue
+++ vue-admin-template/src/views/login/index.vue
@@ -6,20 +6,21 @@
       <el-card shadow="never" class="login-card">
         <!--登录表单--> 
     <!-- el-form > el-form-item > el-input -->
-    <el-form>
-      <el-form-item>
-        <el-input placeholder="请输入手机号" />
+    <el-form ref="form" :model="loginForm" :rules="loginRules">
+      <el-form-item prop="mobile">
+        <el-input placeholder="请输入手机号" v-model="loginForm.mobile" />
       </el-form-item>
-      <el-form-item>
-        <el-input placeholder="请输入密码" />
+     
+      <el-form-item prop="password">
+        <el-input placeholder="请输入密码" v-model="loginForm.password" show-password />
       </el-form-item>
-      <el-form-item>
-        <el-checkbox>
+      <el-form-item prop="isAgree">
+        <el-checkbox v-model="loginForm.isAgree">
           用户平台使用协议
         </el-checkbox>
       </el-form-item>
       <el-form-item>
-        <el-button style="width:350px" type="primary">登录</el-button>
+        <el-button style="width:350px" type="primary" @click="login">登录</el-button>
    </el-form-item>
 </el-form>
       </el-card>  
@@ -28,7 +29,66 @@
 </template>
 <script>
 export default {
-  name : "Login"
+  name : "Login",
+  data() {
+    return {
+      loginForm: {
+        mobile: '',
+        password: '',
+        isAgree: false
+      },
+      loginRules: {
+        mobile: [
+        //规则1  
+        {
+          required: true,
+          message: '请输入手机号',
+          //表示焦点离开的时候触发
+          trigger: 'blur'
+        },
+        //规则2
+         {
+          pattern: /^1[3-9]\d{9}$/,
+          message: '手机号格式不正确',
+          trigger: 'blur'
+
+        }],
+        password: [{
+          required: true,
+          message: '请输入密码',
+          trigger: 'blur'
+        }, {
+          min: 6,
+          max: 16,
+          message: '密码长度应该为6-16位之间',
+          trigger: 'blur'
+
+        }],
+        // required只能检查 null "" undefined
+        isAgree: [{
+          //自定义校验规则 规定有rule、value、callback这三个参数
+          //value的值有ture 或者 false 两种
+          //callback函数有点像js中的Promise ， 它成功时执行resolve，失败时执行reject
+          validator: (rule, value, callback) => {
+            // rule规则(这边没有使用到rule)
+            // value检查的数据  true/false
+            // callback 函数 执行这个函数
+            // 成功执行callback 失败也执行callback(错误对象 new Error(错误信息))
+            value ? callback() : callback(new Error('没有勾选用户平台协议'))
+          }
+        }]
+      }
+    }
+  },
+  methods:{
+    login() {
+      this.$refs.form.validate((isOK) => {
+        if (isOK) {
+          alert('校验通过')
+        }
+      })
+    }
+  }
 }
 </script>
 <style lang="scss">
